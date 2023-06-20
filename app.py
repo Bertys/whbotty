4fcb6a1eaced0f4246fc18fcb9d9dbb1887afe1c
Bertys <bertysitu@gmail.com>
2023-06-20 18:08:33 +0200

    lastry
    asdadsas

--- app.py
+++ app.py
@@ -8,60 +8,65 @@ import hashlib
 import hmac
 import base64
 
+
 api_host = 'https://api.kucoin.com'
+
 
 app = Flask(__name__)
 
 
 client = Client(config.API_KEY, config.API_SECRET, config.API_PASSPHRASE)
 
-@app.route('/')
-def welcome():
-    return render_template('index.html')
+
+@app.route("/")
+def index():
+    return "Hello World!"
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
     #print(request.data)
+
     data = json.loads(request.data)
-    
     if data['passphrase'] != config.TV_WEBHOOK:
-        return {
-            "code": "error",
-            "message": "Nice try, invalid passphrase"
-        }
-    postOrder()
+        print("faallasteeee") 
 
-
-def postOrder():
     url = "https://api.kucoin.com/api/v1/orders"
-
     now = int(time.time() * 1000)
-
     data = {"clientOid": "ABB", "side": "buy", "symbol": "BTC-USDT", "type": "market", "size": "0.0001"}
     data_json = json.dumps(data)
     str_to_sign = str(now) + 'POST' + '/api/v1/orders' + data_json
 
     signature = base64.b64encode(hmac.new(config.API_SECRET.encode(
-        'utf-8'), str_to_sign.encode('utf-8'), hashlib.sha256).digest())
+            'utf-8'), str_to_sign.encode('utf-8'), hashlib.sha256).digest())
     passphrase = base64.b64encode(hmac.new(config.API_SECRET.encode(
-        'utf-8'), config.API_PASSPHRASE.encode('utf-8'), hashlib.sha256).digest())
+            'utf-8'), config.API_PASSPHRASE.encode('utf-8'), hashlib.sha256).digest())
 
     headers = {
-        "KC-API-SIGN": signature,
-        "KC-API-TIMESTAMP": str(now),
-        "KC-API-KEY": config.API_KEY,
-        "KC-API-PASSPHRASE": passphrase,
-        "KC-API-KEY-VERSION": "2",
-        "Content-Type": "application/json"
+            "KC-API-SIGN": signature,
+            "KC-API-TIMESTAMP": str(now),
+            "KC-API-KEY": config.API_KEY,
+            "KC-API-PASSPHRASE": passphrase,
+            "KC-API-KEY-VERSION": "2",
+            "Content-Type": "application/json"
     }
 
     try:
-        res = requests.post(
-            url, headers=headers, data=data_json).json()
+            res = requests.post(
+                url, headers=headers, data=data_json).json()
 
-        print(res)
+            print(res)
 
     except Exception as err:
-        print(err)
+            print(err)
 
-
+    if res:
+        return {
+            "code": "success",
+            "message": "order executed"
+        }
+    else:
+        print("order failed")
+        return {
+            "code": "error",
+            "message": "order failed"
+        }
